d5926ca90a1475ea7d50564ccbb847998ef4a392
AstroSheppard <kshep23@umd.edu>
2018-05-01 00:46:46 +0200

    Update wlanalysis.sh
    need to fix script

--- whitelight/wlanalysis.sh
+++ whitelight/wlanalysis.sh
@@ -1,15 +1,15 @@
-; Whitelight curve analysis
+# Whitelight curve analysis
 
-visit='kelt1/visit01'
-direction='forward'
-savename='kelt1f1'
+visit='hatp41/visit01'
+direction='reverse'
+#savename=visit+direction
 transit=False
 
-; Run wave_solution.py
+# Run wave_solution.py
 
-wave=wave_solution(visit, direction , /plotting, savename=savename)
+wave=wave_solution(visit, direction , , savename=savename)
 ;Run run.py, which should import eclipse2017.py, whitelight_eclipse2017.py
 
-run, visit, direction, savefile=savename
+preprocess_whitelight
 
 
