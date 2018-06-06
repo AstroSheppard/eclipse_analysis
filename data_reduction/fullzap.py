eb245b5f05962049e51f1646d299cb034e8af435
AstroSheppard <kshep23@umd.edu>
2018-06-06 13:56:42 -0400

    Add files via upload

--- data_reduction/fullzap.py
+++ data_reduction/fullzap.py
@@ -5,45 +5,47 @@ import numpy as np
 from astropy.io import fits
 from tqdm import tqdm
 from tqdm import trange
+
 from scipy.special import erfinv
 import matplotlib.pyplot as plt
 
-
-def bad_pixels(visit, direction):
-    folder='./zapped2017/'+visit+'/'+direction+'/cr/*.fits'
-    data=glob.glob(folder)
+def bad_pixels(data, headers, errors, raws, savefile):
+    """ Used to fix bad pixels, but that's no 
+    longer an issue with flat field correction.
+    Now, this 1) gets aperture and 2) zeros negative
+    pixels"""
     n=len(data)
     x=np.zeros(3)                   #[apt,min,max]
-    y=np.zeros(3) 
-    xl=np.zeros(3) 
-    yl=np.zeros(3) 
+    y=np.zeros(3)  
     if n > 1:
         ####Find base aperture####
         x,y=aperture(data[1])
         # Use larger aperture to include all light
         update=np.asarray([1,-1,1])
-        xl=x+4*update
-        yl=y+4*update
-        for i,img in tqdm(enumerate(data), desc='Correcting bad pixels'):
-            exposure=fits.open(img)
-            print len(exposure)
-            img_data=exposure[0].data #### is this 0 right?
-            expo=img_data[xl[1]:xl[2], yl[1]:yl[2]]
-            hdr=exposure[0].header
-            exposure.close()
-            ####
-            zapped=pixel_zapping(xl[0], yl[0], expo)
+        xl=x+5*update
+        yl=y+5*update
+        for i,img in enumerate(data):
+            expo=img[xl[1]:xl[2], yl[1]:yl[2]]
+            error=errors[i,xl[1]:xl[2], yl[1]:yl[2]]
+            raw=raws[i, xl[1]:xl[2], yl[1]:yl[2]]
+            np.place(expo, expo<0, 0)
+            zapped=expo
+            #zapped=pixel_zapping(expo)
             # save zapped images in folder named by dataset
-            filename = './zapped2017/'+ visit + '/'+direction+'/final/' + "%03d"%i +'.zap.fits'
-            fits.writeto(filename,zapped, header=hdr, overwrite=True)
+            hdr=fits.Header.fromstring(headers[i], sep='\\n')
+            filename=savefile+ "%03d.fits" % i
+            # Add data mask file
+            prim=fits.PrimaryHDU(zapped.data, header=hdr)
+            ext=fits.ImageHDU(zapped.mask.astype(int))
+            err=fits.ImageHDU(error)
+            r=fits.ImageHDU(raw)
+            hdul=fits.HDUList([prim, ext, err,r])
+            hdul.writeto(filename, overwrite=True)
 
-def aperture(data):
-    """Given a sample exposure, return the basic aperture."""
-    # Read in list of fits files, return the x and y apertures
-    exp = fits.open(data)
-    exposure=exp[0].data
+def aperture(exposure):
+    """Given a sample exposure data, return the basic aperture."""
+  
     xlen,ylen = exposure.shape
-    exp.close()
     center=np.zeros(2).astype(int)
     exposure_xcen = exposure[(xlen/2),:]
     
@@ -77,53 +79,82 @@ def aperture(data):
     return (np.vstack((x,y)))
 
 
-def pixel_zapping(xapt, yapt, allspec, plot=False):
+def pixel_zapping(allspec, plot=False):
     """ Removes bad pixels by comparing each pixel
-    to its column's median."""
-    nloop=3                       
-    # Pixels expected to be outside sigma factor in image
-    nextra=5.   
+    to its column's median. This is outdated. No bad pixels
+    exist after flat field fix. Keep nloop at 0"""
+    nloop=0                       
+    # Pixels expected to be outside sigma factor in image   
     np.place(allspec, allspec<0, 0)
     print 'Negative pixels = ', np.sum(allspec<0)
 
+    xapt, yapt=allspec.shape
     # Zoom in to ignore lower-flux pixels in median calculation
-    xapt1=xapt-4 
-   
-
-    inp=1-nextra/4./yapt/xapt1
+    xapt1=xapt-8
+    nextra=5.
+    inp=1-nextra/yapt/xapt1
     n=(2.)**(.5)*erfinv(inp)
     # Sigma rejection factor chosen such that only 5 pixels in image can be expected
     # to naturally lie outside the sigma range. I then can correct the pixels
     # outside the sigma range without worry about overcorrecting
 
-    for j in trange(nloop):       
+    for j in range(nloop):       
         # Take care of any other bad pixels by comparing each pixel to the
         # median of it's own column
-        allspec1=allspec[4:-4,:]
-        column_med=np.median(allspec1,axis=0)
-        col_med=np.broadcast_to(column_med, (2*xapt, 2*yapt))
-        column_med=np.broadcast_to(column_med, (2*xapt1, 2*yapt))
-        sigma=np.sqrt(np.sum((allspec1-column_med)**2,axis=0)/(2*xapt1))
-        sigma=np.broadcast_to(sigma, (2*xapt, 2*yapt))
+        allspec1=allspec.copy()
+        allspec1.mask[0:4,:]=True
+        allspec1.mask[-4:,:]=True
+        column_med=np.ma.median(allspec1,axis=0)
+        col_med=np.broadcast_to(column_med, allspec.shape)
+        denom=xapt-np.sum(allspec1.mask, axis=0)
+        sigma=np.sqrt(np.sum((allspec1-col_med)**2,axis=0)/denom)
+        sigma=np.broadcast_to(sigma, allspec.shape)
         dif=np.abs(allspec-col_med)
+
         # Account for edge effects: If the median of the row is much much
         # different then the median of all rows, then don't change the pixels
-        row_m=np.broadcast_to(np.median(allspec, axis=1), (2*yapt, 2*xapt)).T
-        ycut=np.median(row_m)*.9
-        yhigh=np.median(row_m)*1.1
+        row_m=np.broadcast_to(np.ma.median(allspec, axis=1), (yapt, xapt)).T
+        ycut=np.ma.median(row_m)*.9
+        yhigh=np.ma.median(row_m)*1.1
         # Replace the bad pixels with the median of their column
         index=(dif > n*sigma)* (row_m >= ycut) * (row_m <= yhigh)
-     
-        allspec[index]=col_med[index]
-        print 'Bad pixels found =',np.sum(index)
+        allspec.data[index]=col_med[index]
+    #print 'Bad pixels found =',np.sum(index)
+    return allspec
 
-    zapped=allspec
-    return zapped
+def zapped(allspec):
+        """Input is 3D numpy array of all bkg-removed exposure. 
+        Median values and sigma cuts are used to remove cosmic rays"""
+        #dims=np.empty_like(allspec)
+        #aspec=allspec.copy()
+        nspec, nspat, nwave = allspec.shape
+  	nloop = [10,8]                      
+        
+        rows=np.ma.median(allspec, axis=2)
+        medians=np.tile(np.ma.median(allspec, axis=0),(nspec, 1,1))
+        ### Median sigma check
+        #sigmas=np.sqrt(np.sum((allspec-medians)*(allspec-medians), axis=0)/(nspec-np.sum(allspec.mask, axis=0)))
+        #np.place(sigmas, sigmas==0, 1)
+        #crs=np.abs(allspec-medians)/sigmas
+        ###
+        sigma=np.tile(allspec.std(axis=0), (nspec, 1, 1))
+        np.place(sigma, sigma==0, 1)
+        crs=allspec.anom(axis=0)/sigma
+        uneven_scan_row=np.moveaxis(np.tile(np.abs(rows.anom(axis=0)/rows.std(axis=0))>3
+                                            , (nwave,1,1)), 0, 2)
+        
+  	for sigma in nloop:      
+            nzap = 0
+            index=(crs>sigma) * (~uneven_scan_row)
+            nzap=np.ma.sum(index)    
+            allspec.data[index]=medians[index]
+     	    print 'Cosmic rays corrected %d' % nzap     
+        return allspec
 
-
-if len(sys.argv) != 3:
-    sys.exit('Use python2 [fullzap.py] [planet] [visit]')
-else:
-    visit=sys.argv[1]+'/'+sys.argv[2]
-    bad_pixels(visit, 'forward')
-    bad_pixels(visit, 'reverse')
+if __name__=='__main__':
+    if len(sys.argv) != 3:
+        sys.exit('Use python2 [fullzap.py] [planet] [visit]')
+    else:
+        visit=sys.argv[1]+'/'+sys.argv[2]
+        bad_pixels(visit, 'forward')
+        bad_pixels(visit, 'reverse')
